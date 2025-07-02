08c716a78715a6b7317cff03b70c21fbf7d50d70
Carlos De la cruz <srnibbles@Carloss-MacBook-Air.local>
2025-07-02 16:59:19 -0700

    api: add update fuction logic

--- main.py
+++ main.py
@@ -21,9 +21,72 @@ def root():
 
 @app.route('/function', methods=['POST'])
 def create_function():
+    func_id = save_function(request)
+
+    return jsonify({"message": "Function created", "id": func_id})
+
+@app.route('/function/<fid>', methods=['POST','GET'])
+def get_function(fid):
+    if request.method == 'POST':
+        fid = save_function(request)
+    elif fid not in functions:
+        return jsonify({"error": "Function not found"}), 404
+    return jsonify(functions[fid])
+
+@app.route('/test/<id>', methods=['POST'])
+def test_function(id):
+    if id not in functions:
+        return jsonify({"error": "Function not found"}), 404
+
+    func_info = functions[id]
+    func_path = func_info['path']
+
+    return jsonify({
+        'stdout': 'output',
+        'stderr': 'error',
+        'exit_code': 'code',
+        'logs': ['log1', 'log2', 'log3']
+    })
+
+    # try:
+    #     result = subprocess.run(
+    #         ['sudo','./run_microvm.sh', func_path],
+    #         stdout=subprocess.PIPE,
+    #         stderr=subprocess.PIPE,
+    #         timeout=10
+    #     )
+
+    #     logs = []
+
+    #     with open('{}/vm-log.txt'.format(func_path),'r') as f:
+    #         logs.append(f.readline())
+    #         logs.append(f.readline())
+    #         logs.append(f.readline())
+
+    #         capture = False
+    #         for l in f:
+    #             l = l.strip()
+    #             if l == '[+] Booted into microVM':
+    #                 capture = True
+    #             if capture == True:
+    #                 logs.append(l)
+    #             if l == '[+] Shutting down...':
+    #                 capture = False
+    #                 break
+
+    #     return jsonify({
+    #         'stdout': result.stdout.decode(),
+    #         'stderr': result.stderr.decode(),
+    #         'exit_code': result.returncode,
+    #         'logs': logs
+    #     })
+    # except subprocess.TimeoutExpired:
+    #     return jsonify({'error':'Function execution timeout'}), 504
+
+def save_function(request):
     data = json.loads(request.data.decode('utf-8'))
     mem = data.get('memory', '128')
-    func_id = str(uuid.uuid4())
+    func_id = data.get('id', str(uuid.uuid4()))
     func_path = os.path.join(FUNCTIONS_DIR, func_id)
     os.makedirs(func_path, exist_ok=True)
 
@@ -35,56 +98,8 @@ def create_function():
     functions[func_id] = {
         "id": func_id,
         "memory": mem,
-        "path": func_path
+        "path": func_path,
+        "code": data['code'],
+        "event": data['event']
     }
-
-    return jsonify({"message": "Function created", "id": func_id})
-
-@app.route('/function/<id>')
-def get_function(func_id):
-    if func_id not in functions:
-        return jsonify({"error": "Function not found"}), 404
-    return jsonify(functions[func_id])
-
-@app.route('/test/<id>', methods=['POST'])
-def test_function(id):
-    if id not in functions:
-        return jsonify({"error": "Function not found"}), 404
-
-    func_info = functions[id]
-    func_path = func_info['path']
-
-    try:
-        result = subprocess.run(
-            ['sudo','./run_microvm.sh', func_path],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            timeout=10
-        )
-
-        logs = []
-
-        with open('{}/vm-log.txt'.format(func_path),'r') as f:
-            logs.append(f.readline())
-            logs.append(f.readline())
-            logs.append(f.readline())
-
-            capture = False
-            for l in f:
-                l = l.strip()
-                if l == '[+] Booted into microVM':
-                    capture = True
-                if capture == True:
-                    logs.append(l)
-                if l == '[+] Shutting down...':
-                    capture = False
-                    break
-
-        return jsonify({
-            'stdout': result.stdout.decode(),
-            'stderr': result.stderr.decode(),
-            'exit_code': result.returncode,
-            'logs': logs
-        })
-    except subprocess.TimeoutExpired:
-        return jsonify({'error':'Function execution timeout'}), 504
+    return func_id
